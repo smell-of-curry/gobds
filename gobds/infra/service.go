61742d4d57b170b7eb08ea7c7f6b900f74a53de4
glancist <63142905+glancist@users.noreply.github.com>
2025-05-28 08:13:52 +0100

    gobds: Enable authentication from listener, remove identity service.
    I regret overlooking that this approach has been possible all along

--- gobds/infra/service.go
+++ gobds/infra/service.go
@@ -2,10 +2,8 @@ package infra
 
 import (
 	"github.com/smell-of-curry/gobds/gobds/service/claim"
-	"github.com/smell-of-curry/gobds/gobds/service/identity"
 )
 
 var (
-	IdentityService *identity.Service
-	ClaimService    *claim.Service
+	ClaimService *claim.Service
 )
